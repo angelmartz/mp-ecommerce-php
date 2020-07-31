f81854386a4af0481f040d803295d9d1a2e41236
Angel Martz <angel.martz24@gmail.com>
2020-07-31 13:11:13 -0500

    testin

--- ipn.php
+++ ipn.php
@@ -3,5 +3,8 @@ require_once 'bootstrap.php';
 // Testing only not for production
 $payment = fopen("pagos.txt", "a");
 $json = file_get_contents('php://input');
+fwrite($payment, "================" . PHP_EOL);
 fwrite($payment, $json);
+fwrite($payment, "================ POST" . PHP_EOL);
+fwrite($payment, $_POST);
 fclose($payment);
