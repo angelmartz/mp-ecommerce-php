8fcf8e76d57514c9ed2e24f850e13f5aef4445e8
Angel Martz <angel.martz24@gmail.com>
2020-07-31 11:05:29 -0500

    Added config variables

--- config.php
+++ config.php
@@ -1,6 +1,8 @@
 <?php
 /* Config Variables from Heroku Enviroments */
 $config = [
+    'site_url' => getenv('SITE-URL'),
+    'external_reference' => getenv('EMAIL'),
     'integrator-id' => getenv('INTEGRATOR-ID'),
     'public-key' => getenv('PUBLIC-KEY'),
     'access-token' => getenv('ACCESS-TOKEN')
