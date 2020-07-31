40e744ce6e56e7a2750c9d4ea25eed8cd1ea93fe
Angel Martz <angel.martz24@gmail.com>
2020-07-31 12:58:42 -0500

    Webhook log

--- ipn.php
+++ ipn.php
@@ -1,7 +1,7 @@
 <?php 
 require_once 'bootstrap.php';
-// Testing only
-$payment = fopen("pagos.txt", "wb");
+// Testing only not for production
+$payment = fopen("pagos.txt", "a");
 $json = file_get_contents('php://input');
-fwrite($payment, print_r($json, true));
+fwrite($payment, $json);
 fclose($payment);
